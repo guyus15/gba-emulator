5288118a2fb33d9f73b8fb41995d87dc833b8908
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-05-26 15:51:04 +0100

    Added implementation of the set_state() function

---
+++ arm.c
@@ -0,0 +1,23 @@
+#include "arm.h"
+
+void set_state(arm_t *arm, state_e state)
+{
+    arm->state = state;
+
+    if (arm->state == ARM)
+    {
+        // In ARM state, r13 is used as the Stack Pointer (SP), r14 is used as
+        // the subroutine Link Register (LR), and r15 holds the Program
+        // Counter (PC).
+        arm->SP = arm->general_registers[ARM_STATE_SP_INDEX];   
+        arm->LR = arm->general_registers[ARM_STATE_LR_INDEX];
+        arm->PC = arm->general_registers[ARM_STATE_PC_INDEX];
+    } else if (arm->state == THUMB)
+    {
+        // Temporary values.
+        arm->SP = 0;
+        arm->LR = 0;
+        arm->PC = 0;    
+    }
+}
+
