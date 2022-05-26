ab58378e9b321bd664d5be096357781d8498d54f
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-05-26 14:06:59 +0100

    Added a before_each function to be called before each test

--- arm_test.c
+++ arm_test.c
@@ -1,11 +1,22 @@
 #include <unitc.h>
 #include <arm.h>
 
+#include <stblib.h>
+
 arm_t arm;
 
 static void before_each()
 {
+	// Zero the registers
 	
+	memset(arm->general_registers, 0, GENERAL_REGISTERS_SIZE);	
+	memset(arm->status_registers, 0, STATUS_REGISTERS_SIZE);
+
+	arm->SP = 0;
+	arm->LR = 0;
+	arm->PC = 0;
+	arm->CPSR = 0;
+	arm->SPSR = 0;
 }
 
 int main(int argc, char *argv[])
