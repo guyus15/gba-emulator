59d2db70967faeecf3e56030fc8610f5e6f996df
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-05-28 16:02:18 +0100

    Changed before_each() function of the arm_test to call the ARM initialise() function

--- tests/arm_test.c
+++ tests/arm_test.c
@@ -7,16 +7,9 @@ arm_t arm;
 
 static void before_each()
 {
-    // Zero the registers
-	
-    memset(arm.general_registers, 0, GENERAL_REGISTERS_SIZE);	
-    memset(arm.status_registers, 0, STATUS_REGISTERS_SIZE);
+    // Call the ARM processor initialise_arm() function
 
-    arm.SP = 0;
-    arm.LR = 0;
-    arm.PC = 0;
-    arm.CPSR = 0;
-    arm.SPSR = 0;
+    initialise_arm(&arm);
 }
 
 // Test 1
